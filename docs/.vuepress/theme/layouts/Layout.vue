5b6ef07d0610f210bc0936108f4e33a8d717b3e4
zhangailing <zhangailing@enn.cn>
2022-07-22 14:46:37 +0800

    添加了侧边栏

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,16 +1,16 @@
 <template>
   <section>
     <banner></banner>
-      <el-header class="header-wrap">
-        <h1>alenzhang的博客</h1>
-        <menubar mode="horizontal" :menus="navs"></menubar>
-      </el-header>
-      <el-container>
-        <el-aside width="260px">
-          <menubar :menus="siderbar"></menubar>
-        </el-aside>
-        <Content />
-      </el-container>
+    <el-header class="header-wrap">
+      <h1>alenzhang的博客</h1>
+      <menubar mode="horizontal" :menus="navs"></menubar>
+    </el-header>
+    <el-container>
+      <el-aside width="260px">
+        <menubar :menus="siderbar"></menubar>
+      </el-aside>
+      <Content />
+    </el-container>
   </section>
 </template>
 <script>
@@ -25,31 +25,39 @@ export default {
   data() {
     return {}
   },
-  computed:{
-    navs(){
+  computed: {
+    navs() {
       return this.$themeConfig.nav
     },
-    siderbar(){
-      let sidebarConfig = this.$site.themeConfig?.sidebar
-      if(!sidebarConfig) return []
-      let key = Object.keys(sidebarConfig).find(item=>this.$route.path.includes(item))
-      if(key){   
-        return this.$site?.pages?.filter(page=>page.path.includes(key))?.map(item=>{
-          const side = sidebarConfig[key]
-          console.log(side)
-          return {...item,link:item.path,text:item.title}
-        })
-      }
-
-    }
-  }
+    siderbar() {
+      let sidebarConfig = { ...this.$site.themeConfig?.sidebar }
+      if (!sidebarConfig) return []
+      let sideKey = Object.keys(sidebarConfig).find(side => this.$route.path.includes(side))
+      const side = sidebarConfig[sideKey]
+      const pages = this.$site?.pages?.filter(page => page.path.includes(sideKey))
+      return side.map(item => {
+        return {
+          ...item,
+          text:item.title,
+          items: item.children.map(child => {
+            const title = child[1]
+            console.log(title)
+            const page = pages.find(page => page.relativePath.includes(child[0])) || {}
+            if (title) {
+              page.title = title
+             
+            }
+            return {...page,text:page.title,link:page.path}
+          }),
+        }
+      })
+    },
+  },
 }
 </script>
 <style lang="stylus" scoped>
 section
   min-width 1080px
- 
-
 .header-wrap
   background #545c64
   display flex
@@ -61,9 +69,7 @@ section
     font-size 26px
 .el-aside
   height 100%
-  background-color: rgb(84, 92, 100);
-
+  background-color rgb(84, 92, 100)
 .el-container
-  height: calc(100vh - 155px - 60px) 
-
+  height calc(100vh - 155px - 60px)
 </style>
