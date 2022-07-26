3e98094327ce1273d72ef5f8a7d72b87795125d4
zhangailing <zhangailing@enn.cn>
2022-07-26 09:19:57 +0800

    修改md

---
+++ docs/.vuepress/components/PreviewDocx.vue
@@ -0,0 +1,35 @@
+<template>
+<ClientOnly>
+  <div class="my-component" ref="preview">
+    预览
+    <input type="file" @change="preview" ref="file">
+  </div>
+  </ClientOnly>
+</template>
+<script>
+import docx from 'docx-preview';
+window.JSZip = require('jszip')
+export default {
+  name:'Promise',
+  data(){
+    return {
+      fileName:123
+    }
+  },
+  methods:{
+    preview(e){
+      docx.renderAsync(this.$refs.file.files[0],this.$refs.preview) // 渲染到页面预览
+    }
+  },
+  mounted(){
+    console.log(this)
+  }
+};
+</script>
+<style lang="styl" scoped>
+.my-component{
+  /* width: 100%;
+  height: 90vh;
+  border: 1px solid #000; */
+}
+</style>
---
+++ docs/senior-js/game/sidebar.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+        title: '核心功能',
+        collapsable: true,
+         sidebarDepth: 1, 
+        children: [
+            '/senior-js/underscore/1.md',
+            '/senior-js/underscore/2.md',
+        ]
+    },
+    {
+        title: '回调对象设计',
+        collapsable: true,
+        children: [
+            '/senior-js/underscore/3.md',
+            '/senior-js/underscore/4.md',
+        ]
+    },
+]
